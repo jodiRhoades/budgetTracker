3893d6374fc3ddb7dd41922875d53aa27895a59e
Jodi Rhoades <jo_kawgirl@yahoo.com>
2020-02-27 18:24:44 -0500

    adjustments to manifest and index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,58 +1,35 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-const FILES_TO_CACHE = [
-    "/",
-    "/db.js",
-    "/index.js",
-    "/style.css",
-    "/manifest.json",
+var CACHE_NAME = 'my-site-cache-v1';
 
-  ];
-  
-  
-  // install
-  self.addEventListener("install", function(evt) {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  
-    self.skipWaiting();
-  });
-  
-  
-  // fetch
-  self.addEventListener("fetch", function(evt) {
-    // cache successful requests to the API
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-  
-              return response;
+var urlsToCache = [
+    '/',
+    '/style.css',
+    '/index.js',
+    '/manifest.json'
+];
+
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
             })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        }).catch(err => console.log(err))
-      );
-  
-      return;
-    }
-
-    evt.respondWith(
-      caches.match(evt.request).then(function(response) {
-        return response || fetch(evt.request);
-      })
     );
-  });
-  
-  
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/index.html");
+                }
+            });
+        })
+    );
+});
+
